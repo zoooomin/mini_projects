6de772e40a6372dc7889b67c3f5ed178531db896
zoooomin <110406542+zoooomin@users.noreply.github.com>
2022-10-10 19:46:21 +0900

    Update alarm_clock.py

--- alarm_clock/alarm_clock.py
+++ alarm_clock/alarm_clock.py
@@ -1,3 +1,10 @@
+##설치할 라이브러리
+'''
+pip3 install playsound
+pip3 install pyobjc
+'''
+##오디오 파일
+###원하는 오디오 파일을 프로젝트와 같은 폴더에 넣어 사용해줘야함
 from datetime import datetime
 from playsound import playsound
 alarm_time = input('enter the time of alarm to be set:HH:MM:SS\n')
